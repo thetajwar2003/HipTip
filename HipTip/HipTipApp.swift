0b29e848f46e587deaadcb973d2f4a9802a84554
thetajwar2003 <rahmantajwar131@gmail.com>
2021-07-02 09:44:43 -0400

    Initial Commit

---
+++ HipTip/HipTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  HipTipApp.swift
+//  HipTip
+//
+//  Created by Tajwar Rahman on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct HipTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
